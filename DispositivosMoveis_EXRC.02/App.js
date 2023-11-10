b60602c15f62823d15e6c6c248993b401318fa61
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2023-11-09 22:06:54 -0300

    Add files via upload

---
+++ DispositivosMoveis_EXRC.02/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Contador from './components/Contador';
+
+const App = () => {
+  return (
+    <View>
+      <Contador />
+    </View>
+  );
+}
+
+export default App;
